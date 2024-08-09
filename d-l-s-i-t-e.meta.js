ec19b04da0f58510e6b8dc896ec335147d9559c4
miuraryota3456 <miuraryota3456@github.com>
2024-08-09 23:34:33 +0700

    init

---
+++ d-l-s-i-t-e.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         d-l-s-i-t-e
+// @namespace    https://github.com/miuraryota3456/dam_chau_sa/raw/main/d-l-s-i-t-e.user.js
+// @version      2024.08.09
+// @description  down d-l-s-i-t-e
+// @author       You
+// @include      /^https:\/\/pl[a@]y\.d[l1]sit[e3]\.c[o0]m.+/
+// @include      /^https:\/\/pl[a@]y\.d[l1]sit[e3]\.c[o0]m\/csr\/vi[e3]wer/
+// @icon         https://raw.githubusercontent.com/miuraryota3456/dam_chau_sa/main/d-l-s-i-t-e.png
+// @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/fast-xml-parser/4.2.7/fxparser.min.js
+// @require      https://cdn.jsdelivr.net/npm/lodash/lodash.min.js
+// @require      https://cdn.jsdelivr.net/npm/deepdash/browser/deepdash.min.js
+// @grant        none
+// @downloadURL  https://github.com/miuraryota3456/dam_chau_sa/raw/main/d-l-s-i-t-e.user.js
+// @updateURL    https://github.com/miuraryota3456/dam_chau_sa/raw/main/d-l-s-i-t-e.meta.js
+// ==/UserScript==
