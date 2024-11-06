e4956f47cfc8fc9ea16b9fb4bf9326012fe58f32
miuraryota3456 <miuraryota3456@github.com>
2024-11-07 00:18:54 +0700

    init

--- d-l-s-i-t-e.meta.js
+++ d-l-s-i-t-e.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         d-l-s-i-t-e
 // @namespace    https://github.com/miuraryota3456/dam_chau_sa/raw/main/d-l-s-i-t-e.user.js
-// @version      2024.08.09
+// @version      2024.11.07
 // @description  down d-l-s-i-t-e
 // @author       You
 // @include      /^https:\/\/pl[a@]y\.d[l1]sit[e3]\.c[o0]m.+/
